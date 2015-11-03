17eb5fc1773ff0235a729726ef27297c4c6b04a6
Lambeaux <sflombar@asu.edu>
2015-11-03 11:30:06 -0700

    T3Board Constructor Example
    In this first bit of code, I show you what a constructor is and why it's
    important. I also show you the basic structure of writing your own class
    and then using it in the main program. In this case, we do a simple
    example with constructing a T3Board object that sets all of its
    available play spaces to the "available" state.

--- src/T3Board.java
+++ src/T3Board.java
@@ -6,5 +6,42 @@
  */
 public class T3Board
 {
+    /*
+    This is a 2-dimensional array that defines our game board.
+    Essentially, a 2D array is like a table in Excel. We can
+    query against rows and columns. A 3D array would be like a
+    cube, a 4D array like an animating cube, etc.
+    */
+    private T3State[][] gameBoard;
+
+    /*
+    The following integer specifies how large our game board will be.
+    Notice we've made this variable and the one above 'private' - which
+    means it CANNOT be accessed outside of the class.
+    */
+    private int gameBoardSize;
+
+    /*
+    Our object constructor. Anytime you make a new class, it is useful
+    to define the constructor, which is a method with the same name as
+    the class without a return type. This code is guaranteed to be called
+    before the object can be used, so it's a great place for initialization.
+    Here: we choose the default tic-tac-toe board size of 3 and initialize
+    all spaces to 'Available'.
+    */
+    public T3Board()
+    {
+        gameBoardSize = 3;
+        gameBoard = new T3State[gameBoardSize][gameBoardSize];
+
+        for (int i = 0; i < gameBoardSize; i++)
+        {
+            for (int j = 0; j < gameBoardSize; j++)
+            {
+                gameBoard[i][j] = T3State.Available;
+            }
+        }
+    }
+
 
 }
--- src/T3Game.java
+++ src/T3Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by Lambeaux on 11/3/2015.
  * This will be our primary class for the entire game. It holds our
@@ -6,8 +8,47 @@
  */
 public class T3Game
 {
+    /*
+    In our game class, we have defined a board and a scanner. Remember
+    that we use the scanner to get input. Somewhere in the Java library,
+    there is a class called 'Scanner' that holds the code to make the
+    scanner work. Specifically, it can be found in the package that we
+    imported at the top of this file: "import java.util.Scanner;" --
+    that's where the Scanner class lives!
+
+    There is a method or function in the scanner class
+    called 'nextLine()' that does something meaningful.
+    See label [3] where we invoke this function.
+
+    Currently our T3Board class only has a constructor. It doesn't have
+    any invokable functions. In the next commit I make to GitHub, this
+    will change.
+    */
+    private static T3Board MyGameBoard;
+    private static Scanner MyInputReader;
+
+    
+    /*
+    Here in main we provide some test code for constructing our own
+    T3Board object. Whenever we use the keyword 'new', we are allocating
+    memory for the object's variables, which are stored together, AND we
+    invoke our object's constructor. That was the code in the T3Board file
+    that loops through all our states and sets them to Available.
+    */
     public static void main(String[] args)
     {
+        // [1] Notice how the constructor for a Scanner takes an argument,
+        // which is provided in parenthesis. Our constructor did not define
+        // any arguments in T3Board, so we don't need to provide any.
+        MyGameBoard = new T3Board();
+        MyInputReader = new Scanner(System.in);
 
+        // [2] Once this line prints to the console, we know that the
+        // above lines have finished executing.
+        System.out.println("We have created a new board. ");
+
+        // [3] Here we invoke the scanner's 'nextLine()' function. This
+        // will keep the program from exiting until you hit ENTER.
+        MyInputReader.nextLine();
     }
 }
